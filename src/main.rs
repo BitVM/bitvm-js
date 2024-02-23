b6534725b8277035f0ad066adcbb7852010d04ef
Lukas <lucidLuckylee@users.noreply.github.com>
2024-02-23 06:43:08 +0100

    Add pseudo opcodes

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
 mod opcodes;
-use bitcoin::blockdata::opcodes::all::{OP_ADD, OP_PICK, OP_SWAP};
-use bitcoin::{blockdata::script::Script, Opcode};
 use bitcoin_script::bitcoin_script;
-use opcodes::pushable::{self, Pushable};
-use opcodes::pseudo::unroll;
+use opcodes::pushable;
+use opcodes::pseudo::OP_4PICK;
+
+use crate::opcodes::pseudo::op_2k_mul;
 
 
-pub fn OP_7ROLL() -> Vec<Opcode> {
-    vec![OP_SWAP, OP_ADD, OP_PICK]
-}
-
-pub fn OP_4ROLL() -> bitcoin::ScriptBuf {
-    bitcoin_script! {OP_7ROLL OP_PICK }
-}
 
 fn main() {
-    let y = 13;
+    //let y = 13;
     //let my_script = bitcoin_script! {
     //    <(|x: i32| -> i32 {x + y} )(7)> OP_4ROLL
     //};
     
+    //let my_script = bitcoin_script!{
+    //    <unroll(18, |i| bitcoin_script!{ OP_4ROLL <i> <y>})>
+    //};
+
     let my_script = bitcoin_script!{
-        <unroll(18, |i| bitcoin_script!{ OP_4ROLL <i> <y>})>
+        <op_2k_mul(8)>
+        OP_4PICK
     };
 
     //let my_script = bitcoin_script!{OP_4ROLL};
--- src/opcodes/pseudo.rs
+++ src/opcodes/pseudo.rs
@@ -1,4 +1,8 @@
+#![allow(non_snake_case)]
+#![allow(dead_code)]
 use super::pushable::{self, Pushable};
+use bitcoin::ScriptBuf;
+use bitcoin_script::bitcoin_script;
 
 pub fn unroll<F, T>(count: u32, closure: F) -> Vec<T>
 where
@@ -11,4 +15,101 @@ where
         result.push(closure(i))
     }
     result
+}
+
+/// OP_4PICK
+/// The 4 items n back in the stack are copied to the top.
+pub fn OP_4PICK() -> ScriptBuf {
+    bitcoin_script! { OP_ADD
+    OP_DUP  OP_PICK OP_SWAP
+    OP_DUP  OP_PICK OP_SWAP
+    OP_DUP  OP_PICK OP_SWAP
+    OP_1SUB OP_PICK }
+}
+
+/// OP_4ROLL
+/// The 4 items n back in the stack are moved to the top.
+pub fn OP_4ROLL() -> ScriptBuf {
+    bitcoin_script! {
+        <4> OP_ADD
+        OP_DUP  OP_ROLL OP_SWAP
+        OP_DUP  OP_ROLL OP_SWAP
+        OP_DUP  OP_ROLL OP_SWAP
+        OP_1SUB OP_ROLL
+    }
+}
+
+pub fn OP_4DUP() -> ScriptBuf {
+    bitcoin_script! {
+    OP_2OVER OP_2OVER
+    }
+}
+
+pub fn OP_4DROP() -> ScriptBuf {
+    bitcoin_script! {
+        OP_2DROP OP_2DROP
+    }
+}
+
+pub fn OP_4SWAP() -> ScriptBuf {
+    bitcoin_script! {
+    <7> OP_ROLL <7> OP_ROLL
+    <7> OP_ROLL <7> OP_ROLL
+    }
+}
+
+/// Puts the top 4 items onto the top of the alt stack. Removes them from the main stack.
+pub fn OP_4TOALTSTACK() -> ScriptBuf {
+    bitcoin_script! {
+        OP_TOALTSTACK OP_TOALTSTACK OP_TOALTSTACK OP_TOALTSTACK
+    }
+}
+
+/// Puts the top 4 items from the altstack onto the top of the main stack. Removes them from the alt stack.
+pub fn OP_4FROMALTSTACK() -> ScriptBuf {
+    bitcoin_script! {
+        OP_FROMALTSTACK OP_FROMALTSTACK OP_FROMALTSTACK OP_FROMALTSTACK
+    }
+}
+
+///
+/// Multiplication by Powers of 2
+///
+/// The input is multiplied by 2
+pub fn OP_2MUL() -> ScriptBuf {
+    bitcoin_script! {
+        OP_DUP OP_ADD
+    }
+}
+
+/// The input is multiplied by 4
+pub fn OP_4MUL() -> ScriptBuf {
+    bitcoin_script! {
+        OP_DUP OP_ADD OP_DUP OP_ADD
+    }
+}
+
+/// The input is multiplied by 2**k
+pub fn op_2k_mul(k: u32) -> ScriptBuf {
+    bitcoin_script! {
+        <unroll(k, |_| OP_2MUL())>
+    }
+}
+
+/// The input is multiplied by 16
+pub fn OP_16MUL() -> ScriptBuf {
+    bitcoin_script! {
+        OP_DUP OP_ADD OP_DUP OP_ADD
+        OP_DUP OP_ADD OP_DUP OP_ADD
+    }
+}
+
+/// The input is multiplied by 256
+pub fn OP_256MUL() -> ScriptBuf {
+    bitcoin_script! {
+        OP_DUP OP_ADD OP_DUP OP_ADD
+        OP_DUP OP_ADD OP_DUP OP_ADD
+        OP_DUP OP_ADD OP_DUP OP_ADD
+        OP_DUP OP_ADD OP_DUP OP_ADD
+    }
 }
