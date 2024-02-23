6dccb2d3b0c1f95c871fab4656a995d7cf37aa77
Lukas <lucidLuckylee@users.noreply.github.com>
2024-02-23 04:40:45 +0100

    Showcase bitcoin-script macro functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 mod opcodes;
-use crate::opcodes::{Opcode, OP_ADD};
-use crate::opcodes::pseudo::{OP_4PICK, OP_2MUL};
+use bitcoin::blockdata::opcodes::all::{OP_ADD, OP_PICK, OP_SWAP};
+use bitcoin::{blockdata::script::Script, Opcode};
+use bitcoin_script::bitcoin_script;
+use opcodes::pushable::{self, Pushable};
+use opcodes::pseudo::unroll;
+
+
+pub fn OP_7ROLL() -> Vec<Opcode> {
+    vec![OP_SWAP, OP_ADD, OP_PICK]
+}
+
+pub fn OP_4ROLL() -> bitcoin::ScriptBuf {
+    bitcoin_script! {OP_7ROLL OP_PICK }
+}
 
 fn main() {
-    println!("{:?}", const_compose!([1,2,3,4, OP_ADD]));
-    const MY_VAR : i32 = 7;
-    println!("{:?}", const_compose!([OP_4PICK, OP_2MUL, OP_ADD, MY_VAR]));
-    println!("{:?}", opcodes::pseudo::OP_4PICK);
+    let y = 13;
+    //let my_script = bitcoin_script! {
+    //    <(|x: i32| -> i32 {x + y} )(7)> OP_4ROLL
+    //};
+    
+    let my_script = bitcoin_script!{
+        <unroll(18, |i| bitcoin_script!{ OP_4ROLL <i> <y>})>
+    };
+
+    //let my_script = bitcoin_script!{OP_4ROLL};
+
+    println!("{:?}", my_script);
 }
